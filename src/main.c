8e66748409fc1003b1ad9897e95096e174f3f1eb
Nishaant NU <nishaant.nu@gmail.com>
2026-02-01 15:01:33 +0530

    Added help option.
    Added ability to get help using the --h option.
    Also added the primitive version of checking for arguments via the
    checkArgs() function.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
+int checkArgs(int ac, char **argv, char *term) {
+  for (int i = 1; i < ac; i++) {
+    if((strcmp(argv[i], term)) == 0) return 0;
+  }
+  return -1;
+}
+
 int main(int argc, char **argv) {
   FILE *fp;
 
@@ -13,12 +20,29 @@ int main(int argc, char **argv) {
   } else {
     if (argc > 1) {
       for (int i = 1; i < argc; i++) {
-        if ((strcmp(argv[i], "-")) == 0) {
+        if (checkArgs(argc, argv, "--h") == 0) {
+          const char *help = "Nishaant's cat implementation!\n"
+                             "Usage: cat [OPTION]... [FILE]...\n"
+                             "ConCATenate FILE(s) to the standard output.\n"
+			     "\nWith no FILE, or when file is -, read standard input.\n";
+
+          printf("%s\n", help);
+          break;
+        }
+        else if ((strcmp(argv[i], "-")) == 0) {
           char ch;
 	  while ((ch = fgetc(stdin)) != EOF) {
 	    printf("%c", ch);
-	  }
-        } else {
+          }
+        } else if ((strcmp(argv[i], "--h")) == 0) {
+          const char *help = "Nishaant's cat implementation!\n"
+                             "Usage: cat [OPTION]... [FILE]...\n"
+                             "ConCATenate FILE(s) to the standard output.\n"
+			     "\nWith no FILE, or when file is -, read standard input.\n";
+
+	  printf("%s", help);
+        }
+        else {
 	  //      printf("Trying to open: %s\n", argv[i]);
 	  fp = fopen(argv[i], "r");
 	  if (fp == NULL) {
