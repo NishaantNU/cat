b04eb553ab6d8e7102f52ce3db93d1790df3463c
Nishaant NU <nishaant.nu@gmail.com>
2026-02-01 16:07:06 +0530

    Added -n option.
    Added the ability to number lines using the -n option.

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 cat:
 	gcc src/main.c -o out/cat
 
+debug:
+	gcc src/main.c -o out/cat_debug -g -Wall -Wextra -O0
 clean:
 	rm out/cat
--- src/main.c
+++ src/main.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
-int checkArgs(int ac, char **argv, char *term) {
-  for (int i = 1; i < ac; i++) {
-    if((strcmp(argv[i], term)) == 0) return 0;
+int checkArgs(int *ac, char **argv, char *term, int removeTerm) {
+  for (int i = 1; i < *ac; i++) {
+    if ((strcmp(argv[i], term)) == 0) {
+      if (removeTerm == 1) {
+        for (int j = i; j < *ac - 1; j++) {
+          argv[j] = argv[j + 1];
+        }
+        argv[*ac - 1] = NULL;
+        (*ac)--;
+      }
+     return 0;
+    }
   }
   return -1;
 }
 
 int main(int argc, char **argv) {
+  int printLineNumber=0;
+  int lineNumber=1;
   FILE *fp;
 
   if (argc == 1) {
@@ -19,39 +30,57 @@ int main(int argc, char **argv) {
 
   } else {
     if (argc > 1) {
+      if (checkArgs(&argc, argv, "-n", 1) == 0) {
+        printLineNumber=1;
+      }
+
       for (int i = 1; i < argc; i++) {
-        if (checkArgs(argc, argv, "--h") == 0) {
-          const char *help = "Nishaant's cat implementation!\n"
-                             "Usage: cat [OPTION]... [FILE]...\n"
-                             "ConCATenate FILE(s) to the standard output.\n"
-			     "\nWith no FILE, or when file is -, read standard input.\n";
+        if (checkArgs(&argc, argv, "--h", 0) == 0) {
+          const char *help =
+              "Nishaant's cat implementation!\n"
+              "Usage: cat [OPTION]... [FILE]...\n"
+              "ConCATenate FILE(s) to the standard output.\n"
+              "\nWith no FILE, or when file is -, read standard input.\n"
+              "\nOptions:\n"
+              "\t-n: Number each line\n";
 
           printf("%s\n", help);
           break;
         }
         else if ((strcmp(argv[i], "-")) == 0) {
+	  int isNewLine=1;
           char ch;
-	  while ((ch = fgetc(stdin)) != EOF) {
-	    printf("%c", ch);
-          }
-        } else if ((strcmp(argv[i], "--h")) == 0) {
-          const char *help = "Nishaant's cat implementation!\n"
-                             "Usage: cat [OPTION]... [FILE]...\n"
-                             "ConCATenate FILE(s) to the standard output.\n"
-			     "\nWith no FILE, or when file is -, read standard input.\n";
+          while ((ch = fgetc(stdin)) != EOF) {
+            if (isNewLine && printLineNumber) {
+              printf("%d ", lineNumber);
+              isNewLine = 0;
+            }
+            printf("%c", ch);
+            if (ch == '\n') {
 
-	  printf("%s", help);
-        }
-        else {
+              lineNumber += 1;
+             isNewLine=1;
+            }
+          }
+        } else {
 	  //      printf("Trying to open: %s\n", argv[i]);
 	  fp = fopen(argv[i], "r");
 	  if (fp == NULL) {
             printf("%s: %s: No such file or dir.\n", argv[0], argv[i]);
             continue;
-	  }
+          }
+          int isNewLine=1;
 	  char ch;
-	  while ((ch = fgetc(fp)) != EOF) {
+          while ((ch = fgetc(fp)) != EOF) {
+            if (isNewLine && printLineNumber) {
+              printf("%d ", lineNumber);
+              isNewLine=0;
+            }
             printf("%c", ch);
+            if (ch == '\n') {
+              lineNumber += 1;
+              isNewLine = 1;
+            }
 	  }
 	  fclose(fp);
 	  //      printf("\n");
