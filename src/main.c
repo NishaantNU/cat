767aa8ae152b8c2aae25a5782364faec7797d40a
Nishaant NU <nishaant.nu@gmail.com>
2026-02-01 14:30:55 +0530

    Read from stdin
    Added the ability to read from stdin. When no argument is given,
    the program reads from stdin. If '-' is passed as an argument, stdin
    is read from.

--- src/main.c
+++ src/main.c
@@ -1,22 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
   FILE *fp;
 
-  if (argc > 1) {
-    for (int i = 1; i < argc; i++) {
-//      printf("Trying to open: %s\n", argv[i]);
-      fp = fopen(argv[i], "r");
-      if (fp == NULL) {
-        printf("%s: %s: No such file or dir.\n", argv[0], argv[i]);
-        continue;
+  if (argc == 1) {
+    char ch;
+    while ((ch = fgetc(stdin)) != EOF) {
+      printf("%c", ch);
+    }
+
+  } else {
+    if (argc > 1) {
+      for (int i = 1; i < argc; i++) {
+        if ((strcmp(argv[i], "-")) == 0) {
+          char ch;
+	  while ((ch = fgetc(stdin)) != EOF) {
+	    printf("%c", ch);
+	  }
+        } else {
+	  //      printf("Trying to open: %s\n", argv[i]);
+	  fp = fopen(argv[i], "r");
+	  if (fp == NULL) {
+            printf("%s: %s: No such file or dir.\n", argv[0], argv[i]);
+            continue;
+	  }
+	  char ch;
+	  while ((ch = fgetc(fp)) != EOF) {
+            printf("%c", ch);
+	  }
+	  fclose(fp);
+	  //      printf("\n");
+        }
       }
-      char ch;
-      while ((ch = fgetc(fp)) != EOF) {
-        printf("%c", ch);
-      }
-      fclose(fp);
-//      printf("\n");
     }
   }
   return 0;
